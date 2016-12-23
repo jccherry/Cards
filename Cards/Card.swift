38aa54e923028acd815880a5fe94ca163c818736
jccherry <jchiaramonte18@regis.org>
2016-12-23 00:08:45 -0500

    initial commit

---
+++ Cards/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Cards
+//
+//  Created by John Chiaramonte on 12/23/16.
+//  Copyright © 2016 John Chiaramonte. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Card {
+    
+    init() {
+        
+    }
+    
+}
---
+++ Cards/Deck.swift
@@ -0,0 +1,18 @@
+//
+//  Deck.swift
+//  Cards
+//
+//  Created by John Chiaramonte on 12/23/16.
+//  Copyright © 2016 John Chiaramonte. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Deck {
+    
+    init() {
+        
+    }
+    
+}
